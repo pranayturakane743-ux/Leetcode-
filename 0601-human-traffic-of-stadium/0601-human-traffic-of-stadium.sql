862271143831de5ea345d99b6faab58cd59d1a64
Pranay Turakane <pranayturakane743@gmail.com>
2026-08-30 12:32:19 +0530

    Time: 489 ms (16.97%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,26 @@
+WITH ConsecutiveGroups AS (
+    SELECT 
+        id, 
+        visit_date, 
+        people,
+        id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM 
+        Stadium
+    WHERE 
+        people >= 100
+)
+SELECT 
+    id, 
+    visit_date, 
+    people
+FROM 
+    ConsecutiveGroups
+WHERE 
+    grp IN (
+        SELECT grp 
+        FROM ConsecutiveGroups 
+        GROUP BY grp 
+        HAVING COUNT(*) >= 3
+    )
+ORDER BY 
+    visit_date ASC;
